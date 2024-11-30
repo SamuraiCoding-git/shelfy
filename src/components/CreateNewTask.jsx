b6f6dd622eee2a8e63e69acd56740de2ca62db04
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2024-11-30 19:21:29 +0300

    Update

--- src/components/CreateNewTask.jsx
+++ src/components/CreateNewTask.jsx
@@ -163,4 +163,4 @@ export default function CreateNewTask({ onClose }) {
             </motion.form>
         </>
     );
-}
+};
--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -107,4 +107,4 @@ export default function DatePicker({ currentDate, onClose, onSave }) {
             </div>
         </>
     );
-}
+};
