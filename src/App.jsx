4f7b82082a851f883ceca5b712200ba22ed49ae2
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2024-12-09 01:11:46 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Navigation from './components/Navigation';
@@ -6,6 +6,7 @@ import Features from "./pages/Features.jsx";
 import Calendar from "./pages/Calendar.jsx";
 import UserProfile from "./pages/Profile.jsx";
 import Leaderboard from "./pages/Leaderboard.jsx";
+import axios from 'axios';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,6 +19,25 @@ const router = createBrowserRouter(
         </Route>
     )
 );
+
+useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+        // Alternatively to what can be set with react-telegram-web-app, you can directly set the following properties:
+        try {
+            window.Telegram.WebApp.requestWriteAccess()
+        } catch (e) {
+            console.log(e)
+        }
+        window.Telegram.WebApp.expand()
+    }
+    axios.post('https://6ac0-46-183-186-2.ngrok-free.app/users', initData)
+        .then(response => {
+            console.log('Response:', response.data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}, [])
 
 function App() {
     const tg = window.Telegram.WebApp;
